46d914901484e00a4e8be47e4d96ff1df683e01d
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2020-08-05 17:53:50 +0200

    Upstream scripts back ports [Step 1]
    Coming from the following PRs (with small customizations for our features):
    
        bitcoin # 6335
        bitcoin # 6424
        bitcoin # 12460
        bitcoin # 13194

--- src/script/sign.cpp
+++ src/script/sign.cpp
@@ -6,9 +6,10 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include "script/sign.h"
 
-#include "primitives/transaction.h"
 #include "key.h"
 #include "keystore.h"
+#include "policy/policy.h"
+#include "primitives/transaction.h"
 #include "script/standard.h"
 #include "uint256.h"
 #include "util.h"
