5ac5129081d48b20297a4b16a6e5376205568eac
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2020-09-24 16:17:06 +0200

    [Model] Wallet interface refactor + code cleanup.
    This is motivated by the different types of balances that we currently have (and the new ones that are coming).
    
    Refactor: Back ported and adapted the WalletBalances wrapper class.
    Cleanup: Removed the not used balance getters from walletModel + some not implemented methods that we had.
    Cleanup: Removed the zerocoin balance related fields from the GUI, we are not showing it anymore and the GUI was still calculating + caching them.

--- src/qt/encocoin/topbar.cpp
+++ src/qt/encocoin/topbar.cpp
@@ -615,17 +615,11 @@ void TopBar::updateDisplayUnit()
         int displayUnitPrev = nDisplayUnit;
         nDisplayUnit = walletModel->getOptionsModel()->getDisplayUnit();
         if (displayUnitPrev != nDisplayUnit)
-            updateBalances(walletModel->getBalance(), walletModel->getUnconfirmedBalance(), walletModel->getImmatureBalance(),
-                           walletModel->getZerocoinBalance(), walletModel->getUnconfirmedZerocoinBalance(), walletModel->getImmatureZerocoinBalance(),
-                           walletModel->getWatchBalance(), walletModel->getWatchUnconfirmedBalance(), walletModel->getWatchImmatureBalance(),
-                           walletModel->getDelegatedBalance(), walletModel->getColdStakedBalance());
+            updateBalances(walletModel->GetWalletBalances());
     }
 }
 
-void TopBar::updateBalances(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& immatureBalance,
-                            const CAmount& zerocoinBalance, const CAmount& unconfirmedZerocoinBalance, const CAmount& immatureZerocoinBalance,
-                            const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance,
-                            const CAmount& delegatedBalance, const CAmount& coldStakedBalance)
+void TopBar::updateBalances(const interfaces::WalletBalances& newBalance)
 {
     // Locked balance. //TODO move this to the signal properly in the future..
     CAmount nLockedBalance = 0;
@@ -635,15 +629,15 @@ void TopBar::updateBalances(const CAmount& balance, const CAmount& unconfirmedBa
     ui->labelTitle1->setText(nLockedBalance > 0 ? tr("Available (Locked included)") : tr("Available"));
 
     // XNK Total
-    QString totalXnk = GUIUtil::formatBalance(balance, nDisplayUnit);
+    QString totalXnk = GUIUtil::formatBalance(newBalance.balance, nDisplayUnit);
 
     // XNK
     // Top
     ui->labelAmountTopXnk->setText(totalXnk);
     // Expanded
     ui->labelAmountXnk->setText(totalXnk);
-    ui->labelPendingXnk->setText(GUIUtil::formatBalance(unconfirmedBalance, nDisplayUnit));
-    ui->labelImmatureXnk->setText(GUIUtil::formatBalance(immatureBalance, nDisplayUnit));
+    ui->labelPendingXnk->setText(GUIUtil::formatBalance(newBalance.unconfirmed_balance, nDisplayUnit));
+    ui->labelImmatureXnk->setText(GUIUtil::formatBalance(newBalance.immature_balance, nDisplayUnit));
 }
 
 void TopBar::resizeEvent(QResizeEvent *event)
