5702eea86e737d3780619cb3295f1edc76c31ff5
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2020-09-27 00:09:11 +0200

    [Model] TransactionRecord decomposeTransaction refactoring
    Initial step towards a cleaner and more understandable TransactionRecord::decomposeTransaction function.
    
    No functional change. Have done mostly code refactoring with some cleanups and improvements.

--- src/qt/transactionrecord.h
+++ src/qt/transactionrecord.h
@@ -4,7 +4,6 @@
 // Copyright (c) 2020 The EncoCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 #ifndef BITCOIN_QT_TRANSACTIONRECORD_H
 #define BITCOIN_QT_TRANSACTIONRECORD_H
 
@@ -122,6 +121,30 @@ public:
     static QList<TransactionRecord> decomposeTransaction(const CWallet* wallet, const CWalletTx& wtx);
 
     /// Helpers
+    static bool decomposeCoinStake(const CWallet* wallet, const CWalletTx& wtx,
+                                   const CAmount& nCredit, const CAmount& nDebit, bool fZSpendFromMe,
+                                   QList<TransactionRecord>& parts);
+
+    static bool decomposeZcSpendTx(const CWallet* wallet, const CWalletTx& wtx,
+                                    const CAmount& nCredit, const CAmount& nDebit, bool fZSpendFromMe,
+                                    QList<TransactionRecord>& parts);
+
+    static bool decomposeP2CS(const CWallet* wallet, const CWalletTx& wtx,
+                                    const CAmount& nCredit, const CAmount& nDebit,
+                                    QList<TransactionRecord>& parts);
+
+    static bool decomposeCreditTransaction(const CWallet* wallet, const CWalletTx& wtx,
+                                    QList<TransactionRecord>& parts);
+
+    static bool decomposeSendToSelfTransaction(const CWalletTx& wtx, const CAmount& nCredit,
+                                    const CAmount& nDebit, bool involvesWatchAddress,
+                                    QList<TransactionRecord>& parts);
+
+    static bool decomposeDebitTransaction(const CWallet* wallet, const CWalletTx& wtx,
+                                                      const CAmount& nDebit, bool involvesWatchAddress,
+                                                      QList<TransactionRecord>& parts);
+
+    static std::string getValueOrReturnEmpty(const std::map<std::string, std::string>& mapValue, const std::string& key);
     static bool ExtractAddress(const CScript& scriptPubKey, bool fColdStake, std::string& addressStr);
     static void loadHotOrColdStakeOrContract(const CWallet* wallet, const CWalletTx& wtx,
                                             TransactionRecord& record, bool isContract = false);
