66e3b62345bb9a2bf8b29a17f85f29c37cc95f60
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2020-11-22 22:06:31 +0100

    [Validation] Introducing Sapling transaction network connection and validations.
    Focused on introducing network flow connection and validations for shielded transactions (primitives and contextual checks), coins cache nullifier and anchor and a test case covering the invalid paths.
    
    This work can be verified running 1798 functional tests suite. sapling_wallet_nullifiers.py, sapling_wallet.py, sapling_wallet_anchorfork.py , sapling_key_import_export.py are all running on top of this work, creating shielded txs, checking balances and generating blocks.
    
    -- A future to do in the area is the mempool validations (nullifiers) connection (stated in 1798 but a little bit of redundancy here doesn't hurt) and continue adding more test coverage for the validation area. Plus, discuss the fee topic further on its own PR, can be dynamic and not fixed as is now. --

--- src/sapling/sapling_transaction.h
+++ src/sapling/sapling_transaction.h
@@ -3,7 +3,6 @@
 // Copyright (c) 2020 The EncoCoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 #ifndef EncoCoin_SAPLING_TRANSACTION_H
 #define EncoCoin_SAPLING_TRANSACTION_H
 
@@ -147,6 +146,12 @@ public:
 
     explicit SaplingTxData() : valueBalance(0), vShieldedSpend(), vShieldedOutput() { }
     explicit SaplingTxData(const SaplingTxData& from) : valueBalance(from.valueBalance), vShieldedSpend(from.vShieldedSpend), vShieldedOutput(from.vShieldedOutput), bindingSig(from.bindingSig) {}
+
+    bool hasBindingSig() const
+    {
+        return std::any_of(bindingSig.begin(), bindingSig.end(),
+                          [](const unsigned char& c){ return c != 0; });
+    }
 };
 
 
--- src/sapling/sapling_validation.h
+++ src/sapling/sapling_validation.h
@@ -3,7 +3,6 @@
 // Copyright (c) 2020 The EncoCoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 #ifndef EncoCoin_SAPLING_VALIDATION_H
 #define EncoCoin_SAPLING_VALIDATION_H
 
@@ -14,7 +13,14 @@ class CValidationState;
 
 namespace SaplingValidation {
 
+/** Context-independent validity checks */
+// Note: for +v2, if the tx has no shielded data, this method returns true.
+// Note2: This function only performs shielded data related checks, it does NOT checks regular inputs and outputs.
+bool CheckTransaction(const CTransaction& tx, CValidationState& state, CAmount& nValueOut, bool fIsSaplingActive);
+bool CheckTransactionWithoutProofVerification(const CTransaction& tx, CValidationState &state, CAmount& nValueOut);
+
 /** Check a transaction contextually against a set of consensus rules */
+// Note: if v5 upgrade wasn't enforced, this method returns true without performing any check.
 bool ContextualCheckTransaction(const CTransaction &tx, CValidationState &state,
                                 const CChainParams &chainparams, int nHeight, bool isMined,
                                 bool sInitBlockDownload);
