a5e5fb05c07125710c63861ec31f8aa01009fa8b
Kopernikus-dev <63426249+Kopernikus1@users.noreply.github.com>
2021-01-31 10:32:27 +0100

    [Masternodes] Don't ban invalid pings before enforcement

--- src/version.h
+++ src/version.h
@@ -24,6 +24,9 @@ static const int GETHEADERS_VERSION = 70077;
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70917;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70918;
 
+//! peers with version older than this, could relay invalid (stale) mn pings
+static const int MIN_PEER_CACHEDVERSION = 70918;
+
 //! masternodes older than this proto version use old strMessage format for mnannounce
 static const int MIN_PEER_MNANNOUNCE = 70913;
 
